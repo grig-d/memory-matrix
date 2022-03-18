ba8ea4f0463d87d4d76b8b3253a9dcbba7a0c20c
Grigoriy Demidovich <grig.demidovich@gmail.com>
2022-03-18 22:24:08 +0200

    colors.js done

---
+++ js/colors.js
@@ -0,0 +1,29 @@
+const colors = [
+  '#da4453',
+  '#e9573f',
+  '#f68b42',
+  '#8cc152',
+  '#37bc9b',
+  '#3bafda',
+  '#4a89dc',
+  '#967adc',
+  '#d770ad',
+  '#e6e9ed',
+  '#aab2bd',
+  '#434a54',
+];
+
+// #da4453 grapefruit-main
+// #e9573f bittersweet-main
+// #f68b42 sunflower-main
+// #8cc152 grass-main
+// #37bc9b mint-main
+// #3bafda aqua-main
+// #4a89dc blue-main
+// #967adc lavander-main
+// #d770ad rose-main
+// #e6e9ed light-gray-main
+// #aab2bd medium-gray-main
+// #434a54 dark-gray-main
+
+export default colors;
