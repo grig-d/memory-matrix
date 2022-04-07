024ffed1b1485485ff8c1e94eaf20fbe53160037
Grigoriy Demidovich <grig.demidovich@gmail.com>
2022-04-08 02:30:18 +0300

    closi icon from svg to inline code

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ ref.next.addEventListener('click', newGame);
 // Difficulty hard = 400, medium = 600, easy = 800
 let msPerQuad = 400;
 
-let game, clicksCount, arrayClicked;;
+let game, clicksCount, arrayClicked;
 updateGameObj();
 newGame();
 
@@ -138,6 +138,11 @@ function clearFigure() {
   game.figure.forEach(el => {
     document.getElementById(el).style.backgroundColor = null;
   });
+  // STATUSBAR start - full color
+  // ref.status;
+  // console.log(game.quads);
+  // game.quads.forEach(el => console.log(el));
+  //
   cursorToggle();
   startClicking();
 }
@@ -177,10 +182,12 @@ function showResult() {
     el => !document.getElementById(el).classList.contains('right'),
   );
   if (arrayWrong.length) {
+    const wrongIconMarkup = `<svg style="width: 38px; height: 38px" viewBox="0 0 24 24"><path fill="#999999" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+</svg>`;
     arrayWrong.forEach(el => {
       const currentQuad = document.getElementById(el);
       currentQuad.style.backgroundColor = null;
-      currentQuad.classList.add('wrong');
+      currentQuad.insertAdjacentHTML('beforeend', wrongIconMarkup);
     });
     const missedIconMarkup = `<svg class="missed" style="width: 38px; height: 38px" viewBox="0 0 24 24"><path fill="${game.color}" d="M9.29,21H12.12L21,12.12V9.29M19,21C19.55,21 20.05,20.78 20.41,20.41C20.78,20.05 21,19.55 21,19V17L17,21M5,3A2,2 0 0,0 3,5V7L7,3M11.88,3L3,11.88V14.71L14.71,3M19.5,3.08L3.08,19.5C3.17,19.85 3.35,20.16 3.59,20.41C3.84,20.65 4.15,20.83 4.5,20.92L20.93,4.5C20.74,3.8 20.2,3.26 19.5,3.08Z" /></svg>`;
     game.figure.forEach(el => {
