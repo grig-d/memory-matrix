28c81659b2aafcd362e06c84c474d6d86f34db9c
Grigoriy Demidovich <grig.demidovich@gmail.com>
2022-04-08 03:03:36 +0300

    status bar indication

--- js/renderStatusBar.js
+++ js/renderStatusBar.js
@@ -1,7 +1,7 @@
 export default function renderStatusBar({ quads }) {
   let statusBar = '';
   for (let i = 0; i < quads; i++) {
-    const statusQuad = `<div id="sb-${i + 1}" class="status-quad"></div>`;
+    const statusQuad = `<div id="${i + 1}-status" class="status-quad"></div>`;
     statusBar += statusQuad;
   }
   return statusBar;
